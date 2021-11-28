0eb5f0af627de677096d94034c01e16135bf0800
koos-burgoyne <koos.burgoyne@gmail.com>
2021-11-28 11:35:34 -0700

    error handling and expanded commenting

--- UBL.py
+++ UBL.py
@@ -6,16 +6,36 @@ import sys
 import sklearn
 
 def main():
-    #if len(sys.argv)==2:
-    if len(sys.argv)==1:
-        #sequences = format.import_fa(sys.argv[1])
-        sequences = format.import_fa("pos-ubl.fa")
-        files = format.import_folder(r"E:\Chris\comp_bio\ubl-data\data")
-        
+    # Check if number of arguments is 3
+    # The first argument is the executable name
+    # The second is whether to read a file or folder
+    # The third is the file/folder name
+    if len(sys.argv)==3:
+        # Set up variable to store file contents, whether multiple files or just one
+        file_contents = None
+        # If the first argument is "file", call the import_fa() function to retrieve the sequences
+        if sys.argv[1] == "file":
+            file_contents = format.import_fa(sys.argv[1])
+        # If the first argument is "folder", read the .fa files in the given folder
+        elif sys.argv[1] == "folder":
+            file_contents = format.import_folder(r"E:\Chris\comp_bio\UBL\data")
+        # If the first argument is neither 'file' nor 'folder', print error message and exit
+        else:
+            print("Error: first argument must be 'file' or 'folder'")
+            for i,arg in enumerate(sys.argv):
+                if i > 0:
+                    print("  arg "+str(i)+": "+sys.argv[i])
+            print("Usage: python3 main.py <file/folder> <filename>")
+            sys.exit()
+    # If the incorrect number of arguments is given, print error and exit
     else:
-        print("Error: 1 arg required <filename>, "+str(len(sys.argv)-1)+" args given")
-        print("Usage: python3 main.py <filename>")
-        quit()
+        print("Error: 2 args required, "+str(len(sys.argv)-1)+" args given")
+        for i,arg in enumerate(sys.argv):
+            if i > 0:
+                print("  arg "+str(i)+": "+sys.argv[i])
+        print("Usage: python3 main.py <file/folder> <filename>")
+        sys.exit()
+
     return
 
 if __name__ == "__main__":
--- data_format.py
+++ data_format.py
@@ -2,37 +2,70 @@
 
 # Import numpy library for use in ...
 import numpy as np
+# Import os library for use in ...
 import os
 
 # Function info here ...
 def import_fa(file_name, return_labels=False):
+    # List storage for file contents (sequences) and sequene labels
     sequences = []
     labels = []
-    file = open(file_name, "r")
+    # Try to open the file as read only
+    try:
+        file = open(file_name, "r")
+    # If the file would not open, print error message and exit
+    except:
+        print("\nError: Could not open file")
+        print("File name given: "+file_name+"\n")
+        raise SystemExit
+    # Iterate over all the lines in the file
     for line in file.readlines():
-        if ">" in line:
+        # If this character is at the start of the line, it is a sequence label
+        if line[0] == ">":
+            # If the argument 'return_labels' is True, append the label to a list
             if return_labels:
                 labels.append(str(line)[1:-1])
             else:
                 continue
+        # Append the sequence to the list of sequences
         sequences.append(str(line)[:-1])
+    # If the argument 'return_labels' is True, return arrays of the sequences and labels
     if return_labels:
         return np.array(sequences), np.array(labels)
+    # If False, only return an array of the sequences
     else:
         return np.array(sequences)
 
 # Function info here ...
 def import_folder(folder_path, return_names=False):
+    print("Reading contents of folder: "+folder_path)
+    # List storage for files and file names
     files = []
     file_names = []
-    for file in os.listdir(folder_path):
-        print(file)
-        if "fa" in file:
-            files.append(import_fa(file))
+    # Try read the directory contents to a list
+    try:
+        directory_contents = os.listdir(folder_path)
+    # If directory is not found, print error message and exit
+    except:
+        print("\nError: Could not open directory")
+        print("Directory path given: "+folder_path+"\n")
+        raise SystemExit
+    # Iterate over all files in the given directory
+    for file in directory_contents:
+        # Only read files with the .fa extension
+        if ".fa" in file:
+            print("  "+file)
+            # Call the import_fa() function to read the fasta file
+            # This function returns the sequences in the fasta file
+            # Here the argument 'return_labels' is left as default=False
+            files.append(import_fa(folder_path+'\\'+file))
+            # Add the file contents to the list
             file_names.append(file)
         else:
             continue
+    # If the argument 'return_names' is True, return arrays of file contents and file names
     if return_names:
         return np.array(files, dtype=object), np.array(file_names)
+    # If False, only return a 2D array of the contents of the files
     else:
         return np.array(files, dtype=object)
