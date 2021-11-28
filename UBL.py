47e29022556e61a17d1ad51149bedc88f5f0305e
koos-burgoyne <koos.burgoyne@gmail.com>
2021-11-28 13:36:31 -0700

    more formatting functions added; functions to do with features are incomplete

--- UBL.py
+++ UBL.py
@@ -11,14 +11,17 @@ def main():
     # The second is whether to read a file or folder
     # The third is the file/folder name
     if len(sys.argv)==3:
-        # Set up variable to store file contents, whether multiple files or just one
-        file_contents = None
+        
         # If the first argument is "file", call the import_fa() function to retrieve the sequences
         if sys.argv[1] == "file":
             file_contents = format.import_fa(sys.argv[1])
+        
         # If the first argument is "folder", read the .fa files in the given folder
         elif sys.argv[1] == "folder":
-            file_contents = format.import_folder(r"E:\Chris\comp_bio\UBL\data")
+            folder_contents,file_names = format.import_folder(r"E:\Chris\comp_bio\UBL\data", return_names=True)
+            format.print_stats(folder_contents,file_names)
+            #X, y = format.retrieve_features(folder_contents,fnames=file_names)
+
         # If the first argument is neither 'file' nor 'folder', print error message and exit
         else:
             print("Error: first argument must be 'file' or 'folder'")
@@ -27,6 +30,7 @@ def main():
                     print("  arg "+str(i)+": "+sys.argv[i])
             print("Usage: python3 main.py <file/folder> <filename>")
             sys.exit()
+    
     # If the incorrect number of arguments is given, print error and exit
     else:
         print("Error: 2 args required, "+str(len(sys.argv)-1)+" args given")
--- data_format.py
+++ data_format.py
@@ -29,6 +29,7 @@ def import_fa(file_name, return_labels=False):
                 continue
         # Append the sequence to the list of sequences
         sequences.append(str(line)[:-1])
+    print("    "+str(len(sequences))+" sequences")
     # If the argument 'return_labels' is True, return arrays of the sequences and labels
     if return_labels:
         return np.array(sequences), np.array(labels)
@@ -63,9 +64,89 @@ def import_folder(folder_path, return_names=False):
             file_names.append(file)
         else:
             continue
+    # Return object type array to allow for ragged array
     # If the argument 'return_names' is True, return arrays of file contents and file names
     if return_names:
         return np.array(files, dtype=object), np.array(file_names)
     # If False, only return a 2D array of the contents of the files
     else:
         return np.array(files, dtype=object)
+
+# Function info here ...
+def sequences_to_features(sequences):
+    # The set of features to convert the sequence to
+    feature_lib = {}
+    feature_lib["feat1"] = {}
+    feature_lib["feat2"] = {}
+    num_features = len(feature_lib.keys)
+    # Initialize storage for the new list of features
+    features = [[] for i in range(num_features)]
+    # Iterate over all sequences in 'sequences' array
+    for i,seq in enumerate(sequences):
+        # Iterate over all nucleotides in a sequence
+        for j in seq:
+            # Iterate over all features
+            for k,feature in enumerate(feature_lib.items()):
+                features[i].append()
+    # Return object type array to allow for ragged array
+    return np.array(features, dtype=object)
+
+# Function info here ...
+def pad_features(X):
+    # Find the maximum length of sequence
+    max_len_seq = 0
+    for i in range(len(X)):
+        if len(X[i]) > max_len_seq:
+            max_len_seq = len(X[i])
+    # Create a new array containing the data X padded with zeros so that it is not a ragged array of sequences
+    new_X = np.zeros( (len(X),max_len_seq) )
+    # Iterate over all sequences in the input
+    for i in range(len(X)):
+        # Assign the input sequence to the new padded dataset
+        seq_len = len(X[i])-1
+        new_X[i][:seq_len] = X[i]
+    # Return the padded dataset
+    return new_X
+
+# Function info here ...
+def retrieve_features(files, fnames=None):
+    # Storage for new dataset with revised features and labels
+    sequences_as_features = []
+    labels = []
+    # Iterate over all files in the input 'files' array
+    for file in files:
+        # Iterate over all sequences contained in a file
+        for seqs in file:
+            # Convert the sequence to an instance of data with new features
+            sequences_as_features.append(sequences_to_features(seqs))
+    # Return object type array to allow for ragged array
+    # If the file names are provided, then the labels can be returned according to the file name each sequence came from
+    if fnames is not None:
+        return np.array(sequences_as_features, dtype=object), np.array(labels)
+    # If the file names are not provided, the data is returned unlabeled
+    else:
+        return np.array(sequences_as_features, dtype=object)
+
+# Function info here ...
+def print_stats(files, file_names):
+    print("Data Stats:")
+    for file,fname in zip(files,file_names):
+        max = 0
+        min = 1e6
+        mean_length = 0
+        print("  "+fname)
+        for sequence in file:
+            seq_len = len(sequence)
+            if seq_len > max:
+                max = seq_len
+            if seq_len < min:
+                min = seq_len
+            mean_length += seq_len
+        mean_length /= len(file)
+        std_dev = 0
+        for sequence in file:
+            std_dev += (mean_length - len(sequence))**2
+        print("    Min : "+str(min))
+        print("    Max : "+str(max))
+        print("    Mean: "+str(round(mean_length,3)))
+        print("    StdD: "+str(round(np.sqrt(std_dev/(len(file)-1)),3)))
